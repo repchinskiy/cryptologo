aeb33f4b6c8e18200ff0e4ee78809aa4d4f563ff
repchinskiy <repchinskiy@gmail.com>
2025-01-10 01:08:39 +0200

    Add files via upload

---
+++ hemi.v10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+version="v0.10.0"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+systemctl stop hemi
+cd $HOME
+wget https://github.com/hemilabs/heminetwork/releases/download/$version/heminetwork_"$version"_linux_amd64.tar.gz
+tar -xvf heminetwork_"$version"_linux_amd64.tar.gz && rm heminetwork_"$version"_linux_amd64.tar.gz
+mv -f $HOME/heminetwork_"$version"_linux_amd64/* $HOME/hemi
+rm -rf $HOME/heminetwork_"$version"_linux_amd64
+sudo systemctl daemon-reload
+echo start hemi ... 
+sudo systemctl start hemi
+echo OK 

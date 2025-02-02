032f1bf78b09480be04a3eb5fe93a44ce31a731f
repchinskiy <repchinskiy@gmail.com>
2025-02-02 22:12:25 +0200

    Update cysic.memory.restrict.sh

--- cysic.memory.restrict.sh
+++ cysic.memory.restrict.sh
@@ -4,4 +4,3 @@ echo "+memory" | sudo tee /sys/fs/cgroup/cgroup.subtree_control
 sudo mkdir /sys/fs/cgroup/cysic_node
 echo $((2 * 1024 * 1024 * 1024)) | sudo tee /sys/fs/cgroup/cysic_node/memory.max
 echo $(pgrep -f verifier) | sudo tee /sys/fs/cgroup/cysic_node/cgroup.procs
-

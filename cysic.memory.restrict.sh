6a53c3f4810c848f06b08d91a8bab269767137a6
repchinskiy <repchinskiy@gmail.com>
2025-02-02 22:10:37 +0200

    Add files via upload

--- cysic.memory.restrict.sh
+++ cysic.memory.restrict.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mount | grep cgroup
 echo "+memory" | sudo tee /sys/fs/cgroup/cgroup.subtree_control
 sudo mkdir /sys/fs/cgroup/cysic_node
 echo $((2 * 1024 * 1024 * 1024)) | sudo tee /sys/fs/cgroup/cysic_node/memory.max
-echo $(pgrep -f verifier) | sudo tee /sys/fs/cgroup/memory/cysic_node/cgroup.procs
+echo $(pgrep -f verifier) | sudo tee /sys/fs/cgroup/cysic_node/cgroup.procs
+

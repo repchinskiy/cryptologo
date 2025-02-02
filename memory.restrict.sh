68e0a6b85bf92859e7654b97f927dcb11283262e
repchinskiy <repchinskiy@gmail.com>
2025-02-02 23:08:59 +0200

    Add files via upload

--- memory.restrict.sh
+++ memory.restrict.sh
@@ -3,4 +3,5 @@ mount | grep cgroup
 echo "+memory" | sudo tee /sys/fs/cgroup/cgroup.subtree_control
 sudo mkdir /sys/fs/cgroup/$1
 echo $(($3 * 1024 * 1024 * 1024)) | sudo tee /sys/fs/cgroup/$1/memory.max
-echo $(pgrep -f $2) | sudo tee /sys/fs/cgroup/$1/cgroup.procs
+#echo $(pgrep -f $2) | sudo tee /sys/fs/cgroup/$1/cgroup.procs
+echo $2 | sudo tee /sys/fs/cgroup/$1/cgroup.procs
